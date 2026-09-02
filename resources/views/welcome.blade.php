62f3af7e0e0bae5887d367dbc7af0bfeeb6fa683
AryaN-SEI <aryansingh@orientaloutsourcing.com>
2026-09-02 10:05:44 +0530

    Completed task 1; created a basic laravel webpage using blade template

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+```
+<title>Welcome to Laravel</title>
+
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+```
+
+</head>
+
+<body>
+
+```
+<main class="welcome-container">
+
+    <h1>GREETINGS, EVERYONE!</h1>
+
+    <p>Welcome to our beginner's guide to Laravel.</p>
+
+    <p>
+        This is your first step towards learning Laravel
+        and building modern web applications.
+    </p>
+
+</main>
+```
+
+</body>
+</html>
